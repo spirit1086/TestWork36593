f304a9d464dc4b60c42db832bd376ca1d5d3923f
spirit1086 <spirit1086@mail.ru>
2022-02-05 22:33:24 +0600

    Book: responses

---
+++ backend/app/Api/Book/Resources/BookResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Api\Book\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'author' => $this->author,
+            'book_category' => $this->book_category,
+            'published' => $this->published,
+            'price' => $this->price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
