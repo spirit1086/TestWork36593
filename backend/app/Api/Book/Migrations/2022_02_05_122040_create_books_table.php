9eecfe66e0ed40af78ffb5903bcdc18cd9962437
spirit1086 <spirit1086@mail.ru>
2022-02-05 22:29:56 +0600

    Book: migrations

---
+++ backend/app/Api/Book/Migrations/2022_02_05_122040_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->string('author',255);
+            $table->string('book_category',255);
+            $table->date('published');
+            $table->unsignedInteger('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
