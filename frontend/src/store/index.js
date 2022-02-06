c0aad4f5e156dd212da5dea1a918318ff0ada85c
spirit1086 <spirit1086@mail.ru>
2022-02-07 00:04:26 +0600

    import book module

---
+++ frontend/src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Books from '@/store/modules/books'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    Books
+  }
+})
